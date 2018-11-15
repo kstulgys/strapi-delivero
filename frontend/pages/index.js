a5877e7b1e31f037d72f31e22ceffe271be5714c
kstulgys <karolis.stulgys@gmail.com>
2018-11-15 16:53:50 +1100

    init

---
+++ frontend/pages/index.js
@@ -0,0 +1,10 @@
+/* pages/index.js */
+// import { Button, Alert } from 'reactstrap'
+
+export default () => {
+  return (
+    <div className="blue bb pa4">
+      <h1 className="bb">Hello</h1>
+    </div>
+  )
+}
